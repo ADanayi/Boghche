268aaf65cc66e140234df52b20c98c67d555589a
AbolfazlDanayi <adanayidet@gmail.com>
2019-12-15 14:41:23 +0330

    No auth mode added to gate

--- __gate.py
+++ __gate.py
@@ -3,9 +3,10 @@ from .__entities import add_to_app_t
 
 class BGate(BNet):
 
-    def __init__(self, name, endpoint, app=None, log_func=None):
+    def __init__(self, name, endpoint, app=None, log_func=None, needs_auth=True):
         BNet.__init__(self, endpoint=endpoint, name=name, app=app, log_func=log_func)
         self.__F = {}
+        self.__needs_auth = needs_auth
 
     def get_classes(self=None):
         c = list(BGate.__base__.get_classes())
@@ -17,12 +18,12 @@ class BGate(BNet):
 
     def __handler(self, req):
         try:
-            username = req['auth_user']
-            password = req['auth_password']
+            username = req['auth_user'] if self.__needs_auth else None
+            password = req['auth_password'] if self.__needs_auth else None
             t = req['type']
         except:
             return {'status':'error', 'error':'invalid args'}
-        if not self.auth(username, password):
+        if self.__needs_auth and not self.auth(username, password):
             return {'status':'error', 'error':'unauthorized'}
         if not t in self.__F:
             try:
@@ -38,7 +39,10 @@ class BGate(BNet):
             return {'status':'error', 'error':'internal error'}
 
     def auth(self, user_name, password):
-        return True
+        if not self.__needs_auth:
+            return True
+        else:
+            raise Exception('Auth is not implemented')
 
     def boot(self, app, root):
         global add_to_app_t
@@ -49,6 +53,6 @@ class BGate(BNet):
             endpoint = self.get_endpoint(),
             endpoint_name = 'gate::{}'.format(self.get_name()),
             handler = self.__handler,
-            fetch_auth=True
+            fetch_auth=self.__needs_auth
         )
         self.log2('[Done]')
